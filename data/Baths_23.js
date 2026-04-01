707c2a130f14f14de7d0fbda9fb2b2fe9656abf7
Чавдар Кирилов <156682918+chavdarkirilov@users.noreply.github.com>
2026-04-01 20:51:27 +0300

    Delete data directory

--- data/Baths_23.js
+++
@@ -1,1 +0,0 @@
-var json_Baths_23 = {"type":"FeatureCollection","name":"Baths_23","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"fid":"1","type":"bath","name":"Chifte Hamam"},"geometry":{"type":"Point","coordinates":[25.628320753702571,42.425840408476461]}}]}
